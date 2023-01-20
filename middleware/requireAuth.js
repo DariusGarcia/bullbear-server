991917abeb9cef6a798aec6150b131669cb724ed
Darius Garcia <dariusgarcia888@gmail.com>
2023-01-20 01:11:54 -0800

    added mongoose.type.ObjectId to parse string to ID

--- middleware/requireAuth.js
+++ middleware/requireAuth.js
@@ -1,3 +1,4 @@
+const { ObjectId } = require('mongoose').Types
 const jwt = require('jsonwebtoken')
 const { User } = require('../models/')
 const SECRET_KEY = process.env.SECRET_KEY
@@ -15,7 +16,7 @@ const requireAuth = async (req, res, next) => {
 
     // select property allows you to just select the property you want instead of the whole document
     // e.g. only returns ID instead of username, password, etc.
-    req.user = await User.findOne({ _id }).select('_id')
+    req.user = await User.findOne({ _id: ObjectId(_id) }).select('_id')
     next()
   } catch (error) {
     console.log(error)
